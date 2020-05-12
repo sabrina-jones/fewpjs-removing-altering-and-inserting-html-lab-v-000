8aecf439f62cddca24a42458d1b2588bc553f0c1
Sabrina Jones <sabrina.jones87@gmail.com>
2020-05-12 19:39:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let element = document.createElement('div')
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "test is the champion"
